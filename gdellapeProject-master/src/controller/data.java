8b07cabc707cdbd62e180025601dec28ef6baf40
valerio crescia <valerio.crescia@student.univaq.it>
2018-07-26 12:49:21 +0200

    connection 1

---
+++ gdellapeProject-master/src/controller/data.java
@@ -0,0 +1,49 @@
+package controller;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+
+
+public class data extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/webdb2");
+            Connection con = ds.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM bella");
+
+            while (rs.next()) {
+
+
+                int id = rs.getInt("nome");
+                System.out.println(id);
+            }
+
+
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+
+        } catch (SQLException sq) {
+            sq.printStackTrace();
+
+        }
+
+    }
+}
