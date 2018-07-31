5d7c0030fbbad862fac829088bc3ae579f2f6542
Stefano Decina <stefano.decina@student.univaq.it>
2018-07-31 11:49:11 +0200

    prova per valerio

---
+++ gdellapeProject-master/src/controller/utility/ConnectionFactory.java
@@ -0,0 +1,36 @@
+package controller.utility;
+
+import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory extends HttpServlet {
+
+    private static ConnectionFactory connectionFactory = null;
+
+    public ConnectionFactory() {
+        try {
+            String driverClassName = "com.mysql.jdbc.Driver";
+            Class.forName(driverClassName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+        String connectionUrl = "jdbc:mysql://localhost:3306/student";
+        String dbUser = "root";
+        String dbPwd = "";
+        conn = DriverManager.getConnection(connectionUrl, dbUser, dbPwd);
+        return conn;
+    }
+
+    public static ConnectionFactory getInstance() {
+        if (connectionFactory == null) {
+            connectionFactory = new ConnectionFactory();
+        }
+        return connectionFactory;
+    }
+}
