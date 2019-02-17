040563ab19f45cb5b0e44f37ec1c2403d840471b
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-02-17 10:47:44 +0100

    BOZZA: Creazione Servlet per la gestione delle Impostazioni Account Azienda e Tirocinante

---
+++ gdellapeProject-master/src/controller/adm/BackEndImpostazioniAccountController.java
@@ -0,0 +1,32 @@
+package controller.adm;
+
+import controller.adm.Azienda.ImpostazioniAccountAzienda;
+import controller.baseController;
+import dao.exception.DaoException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BackEndImpostazioniAccountController extends baseController {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.init(request, response);
+        int tipo= (int)request.getAttribute("tipo");
+        if (tipo == 3){
+            ImpostazioniAccountAzienda az = new ImpostazioniAccountAzienda(request, response, getServletContext(), datamodel);
+            try {
+                az.get();
+            } catch (DaoException e) {
+                e.printStackTrace();
+            }
+        }else if (tipo == 2){
+            System.out.println("altro");
+        }else{
+            response.sendRedirect("/500");
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.init(request, response);
+    }
+}
