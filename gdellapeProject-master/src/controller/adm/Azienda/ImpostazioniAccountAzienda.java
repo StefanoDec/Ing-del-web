b33da27aa0b36d560cc6cf91e4cbeab93f90b10f
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-02-18 11:55:53 +0100

    BOZZA: da finire di implementare, per ora i controlli funzionano tutti.

--- gdellapeProject-master/src/controller/adm/Azienda/ImpostazioniAccountAzienda.java
+++ gdellapeProject-master/src/controller/adm/Azienda/ImpostazioniAccountAzienda.java
@@ -6,6 +6,8 @@ import dao.implementation.AziendaDaoImp;
 import dao.implementation.UserDaoImp;
 import model.Azienda;
 import model.User;
+import org.unbescape.html.HtmlEscape;
+import org.unbescape.javascript.JavaScriptEscape;
 import view.TemplateController;
 
 import javax.servlet.ServletContext;
@@ -21,6 +23,8 @@ public class ImpostazioniAccountAzienda {
     protected ServletContext context;
     protected Azienda azienda;
     protected User user;
+    private Boolean modificato;
+    private Boolean sessionescaduta;
 
     public ImpostazioniAccountAzienda(HttpServletRequest request, HttpServletResponse response, ServletContext context, Map<String, Object> datamodel){
         this.datamodel = datamodel;
@@ -29,6 +33,8 @@ public class ImpostazioniAccountAzienda {
         this.context = context;
         this.azienda = null;
         this.user = null;
+        this.modificato = false;
+        this.sessionescaduta = false;
     }
 
     private Azienda ritornaAzienda(SingSessionContoller session, HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -54,52 +60,185 @@ public class ImpostazioniAccountAzienda {
         this.user = ritornUser(azienda);
     }
 
-    private void changeEmail(String email, String emailAttuale){
+    private  void checkModifica(){
+        if(!modificato){
+            this.modificato = true;
+        }
+    }
+
+    private void checkSessioneAndModifica(){
+        if(!modificato || !sessionescaduta) {
+            this.modificato = true;
+            this.sessionescaduta = true;
+        }
+    }
+
+    private Boolean checkPassword(String password){
+        return password.equals(user.getPassword());
+    }
+
+    private Boolean checkEmail(String email){
+        return email.equals(user.getEmail());
+    }
+
+    private void changeEmail(String emailAttuale, String email, String emailRipetuta){
+        System.out.println("le email inserite sono " + email + " " + emailRipetuta);
         String emailAttuleDB = user.getEmail();
         if (emailAttuleDB.equals(emailAttuale)){
-            user.setEmail(email);
-            try {
-                UserDaoImp userDao = new UserDaoImp();
-                userDao.update(user);
-                userDao.destroy();
-            }catch (Exception e){
-                e.printStackTrace();
+            if(email.equals(emailRipetuta)) {
+                if(!emailAttuleDB.equals(email)) {
+                    System.out.println("Modifico l' email ");
+                    user.setEmail(email); // setto la nuova email
+                    checkSessioneAndModifica(); // cambio le flag
+                } else{
+                    System.out.println("l'email attuale coincide con quella del DB");
+                }
+            } else{
+                System.out.println("l'email nuova non coincide con l'altra");
             }
+        } else{
+            System.out.println("l'email attuale non coincide con quella del DB");
         }
+    }
 
+    private void changePassword(String password, String passwordRipetuta, String passwordAttuale){
+        System.out.println("le pwd inserite sono " + password + " " + passwordRipetuta);
+        String passwordAttualeDB = user.getPassword();
+        if (passwordAttualeDB.equals(passwordAttuale)){
+            if(password.equals(passwordRipetuta)) {
+                if (!passwordAttualeDB.equals(password)){
+                    user.setEmail(password); // setto la nuova password
+                    System.out.println("Modifico la pwd");
+                    checkSessioneAndModifica(); // cambio le flag
+                }else {
+                    System.out.println("la password nuova è identica alla Attuale");
+                }
+
+            } else {
+                System.out.println("la password nuova non coincide con l'altra");
+            }
+        }else {
+            System.out.println("la password attuale non coincide con quella del DB");
+        }
+    }
+
+    private void changeDescizione(String email, String password, String
+                                  newDescrizione){
+        System.out.println("la descrizione inserita è: " + newDescrizione);
+        if (checkEmail(email) && checkPassword(password)){
+            if(!newDescrizione.equals(azienda.getDescrizione())){
+                System.out.println("Modifico Descrizione ");
+                newDescrizione = HtmlEscape.escapeHtml5(newDescrizione);
+                System.out.println(newDescrizione);
+                azienda.setDescrizione(newDescrizione);
+                checkModifica();
+            } else {
+                System.out.println("la descrizione è uguale a prima");
+            }
+        } else System.out.println("errore descrizione");
+    }
+
+    private void changeLink(String email, String password, String
+            newLink) {
+        System.out.println("il linck inserito è " + newLink);
+        if (checkEmail(email) && checkPassword(password)) {
+            if(!newLink.equals(azienda.getLink())){
+                System.out.println("Modifico linck ");
+                azienda.setLink(newLink);
+                checkModifica();
+            }else {
+                System.out.println("il link è uguale a prima");
+            }
+        }else System.out.println("errore linck");
     }
 
     private void updateAzienda(){
-        if(request.getParameter("Email").isEmpty()){
-            System.out.println("email vuota");
-        }else{
-            if(!request.getParameter("EmailNuova").isEmpty()) {
-                changeEmail(request.getParameter("EmailNuova"), request.getParameter("Email"));
+        User userAttuale = user;
+        Azienda azziendaAttuale = azienda;
+        String passwordAttuale;
+        String emailAttuale;
+        // Verifico la presenza dell'email e della password Attuale che mi serve per sicurezza
+        if (request.getParameter("EmailAttuale").isEmpty() && request.getParameter("PasswordAttuale").isEmpty()){
+            System.out.println("Modifiche non consentite manchano l'email o la password");
+        }else {
+            emailAttuale = request.getParameter("EmailAttuale");
+            passwordAttuale = request.getParameter("PasswordAttuale");
+            if(emailAttuale.equals(userAttuale.getEmail()) && passwordAttuale.equals(userAttuale.getPassword())){
+
+                // Modifico l'email primaria a cui segue l'INVALIDAZIONE DELLA SESSIONE
+                if(!request.getParameter("Email").isEmpty() && !request.getParameter("EmailRipetuta").isEmpty()) {
+                    changeEmail(emailAttuale, request.getParameter("Email"), request.getParameter("EmailRipetuta"));
+                }
+                else if (request.getParameter("Email").isEmpty()){
+                    System.out.println("email nuova vuota");
+
+                }else  if (request.getParameter("EmailRipetuta").isEmpty()){
+                    System.out.println("email nuova Ripetuta vuota");
+                }
+
+                // Modifico la password a cui segue l'INVALIDAZIONE DELLA SESSIONE
+                if(!request.getParameter("Password").isEmpty() && !request.getParameter("PasswordRipetuta").isEmpty()){
+                    changePassword(request.getParameter("Password"),request.getParameter("PasswordRipetuta"),passwordAttuale);
+                }
+                else if (request.getParameter("Password").isEmpty()){
+                    System.out.println("password nuova vuota");
+
+                }else  if (request.getParameter("PasswordRipetuta").isEmpty()){
+                    System.out.println("password nuova Ripetuta vuota");
+                }
+
+                // Modifico la descrizione
+                if (request.getParameter("Descrizione").isEmpty()){
+                    System.out.println("descrizione vuota");
+                } else {
+                    changeDescizione(emailAttuale,passwordAttuale,request.getParameter("Descrizione"));
+                }
+
+                // Modifico il link
+                if (request.getParameter("Link").isEmpty()){
+                    System.out.println("link vuota");
+                }else {
+                    changeLink(emailAttuale,passwordAttuale,request.getParameter("Link"));
+                }
+
+                // Controllo se devo salvare qualche modifica
+                /*if(modificato){
+                    System.out.println("modificato");
+                    // vedo chi è stato modificato e salvo le modifiche
+
+                    if(user.equals(userAttuale)) {
+                        try {
+                            UserDaoImp userDao = new UserDaoImp();
+                            userDao.update(user);
+                            userDao.destroy();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if(azienda.equals(azziendaAttuale)){
+                        try {
+                            AziendaDaoImp aziendaDao = new AziendaDaoImp();
+                            aziendaDao.updateAzienda(azienda);
+                            aziendaDao.destroy();
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                // Controllo se devo INVALIDARE LA SESSIONE
+                if(sessionescaduta){
+                    System.out.println("sessione scaduta");
+                }
+
+                */
+
+            } else {
+                System.out.println("Modifiche non consentite non conincidono l'email o la password email: " + emailAttuale + "  " + user.getEmail()  + "  password: "+ passwordAttuale + "  " + user.getPassword() );
             }
         }
-        if(request.getParameter("Password").isEmpty()){
-            System.out.println("password vuota");
-        }
-        if (request.getParameter("PasswordNuova").isEmpty()){
-            System.out.println("password nuova vuota");
-        }
-        if (request.getParameter("PasswordNuova1").isEmpty()){
-            System.out.println("password nuova1 vuota");
-        }
-        if (request.getParameter("Descrizione").isEmpty()){
-            System.out.println("descrizione vuota");
-        }
-        if (request.getParameter("Link").isEmpty()){
-            System.out.println("link vuota");
-        }
 
-//        try {
-//            AziendaDaoImp aziendaDao = new AziendaDaoImp();
-//            aziendaDao.updateAzienda(azienda);
-//            aziendaDao.destroy();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
     }
 
     public void get() throws IOException, DaoException {
