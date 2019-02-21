02a408623eddd04e0e4000a5463b03a57f4e725b
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-02-21 15:20:23 +0100

    Creazione BackEndModuliController

---
+++ gdellapeProject-master/src/controller/adm/BackEndModuliController.java
@@ -0,0 +1,18 @@
+package controller.adm;
+
+import controller.baseController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BackEndModuliController extends baseController {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws  ServletException, IOException{
+
+    }
+}
