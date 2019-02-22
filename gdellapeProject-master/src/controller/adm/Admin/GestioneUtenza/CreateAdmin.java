7bba57d33cf4137c91d6e3aa9c1bf0c5062786c8
valerio crescia <valerio.crescia@student.univaq.it>
2019-02-22 09:42:38 +0100

    Creazione user e admin non funziona la validazione

---
+++ gdellapeProject-master/src/controller/adm/Admin/GestioneUtenza/CreateAdmin.java
@@ -0,0 +1,198 @@
+package controller.adm.Admin.GestioneUtenza;
+
+import controller.adm.Admin.BackEndAdminController;
+import controller.adm.Azienda.BackEndAziendaController;
+import controller.baseController;
+import controller.utility.Validation;
+import controller.utility.utility;
+import dao.exception.DaoException;
+import dao.implementation.AdminDaoImp;
+import dao.implementation.UserDaoImp;
+import model.Admin;
+import model.User;
+import view.TemplateController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Servlet che serve a creare un nuovo amministratore quindi dovrà anche creare un nuovo user
+ */
+
+public class CreateAdmin extends baseController {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        super.init(request,response);
+        if(validationUserAndAdmin(request,response))
+        {
+            insertUser(request,response);
+            insertAdmin(request,response);
+            response.sendRedirect("/gestione-utenti");
+        }
+
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+
+
+
+
+    }
+
+
+
+
+
+
+    private void insertUser(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException
+    {
+        try{
+            User user =new User();
+            user.setEmail(request.getParameter("Email"));
+            user.setPassword(request.getParameter("Password"));
+            user.setTipologiaAccount(1);
+
+            UserDaoImp dao = new UserDaoImp();
+            dao.setUser(user);
+            dao.destroy();
+
+        }
+        catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+    }
+    private void insertAdmin(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException
+    {
+        try{
+           UserDaoImp dao = new UserDaoImp();
+           User user=dao.getUserByMail(request.getParameter("Email"));
+           dao.destroy();
+
+
+           Admin admin =new Admin();
+           admin.setNome(request.getParameter("Nome"));
+           admin.setCognome(request.getParameter("Cognome"));
+           admin.setUser(user.getIDUser());
+
+           UserDaoImp dao1 = new UserDaoImp();
+           dao1.setUser(user);
+           dao1.destroy();
+
+        }
+        catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+    }
+    private Boolean validationUserAndAdmin(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException
+    {
+        try {
+
+            Map<String, Object> map = new HashMap<>();
+
+            if (request.getParameter("Email") == null) {
+                map.put("ErroreEmail", "Email non presente");
+
+            } else if (!(Validation.isValidEmailAddress(request.getParameter("Email")))) {
+                map.put("ErroreEmail", "Email non conforme");
+            } else if (Validation.isStoredThisAddress(request.getParameter("Email"))) {
+                map.put("ErroreEmail", "Email gia presente");
+            }
+
+
+            if (request.getParameter("Password") == null) {
+                map.put("ErrorePassword", "Password Non Presente");
+            } else if (!(request.getParameter("Password").equals(request.getParameter("ConfermaPassword")))) {
+                map.put("ErrorePassword", "Password differenti");
+            } else if (request.getParameter("Password").length() > 50) {
+                map.put("ErrorePassword", "Password troppo lunga");
+            }
+
+            if (request.getParameter("ConfermaPassword") == null) {
+                map.put("ErroreConfermaPassword", "Password Non Presente");
+            }
+
+            if(request.getParameter("Nome")==null)
+            {
+                map.put("ErroreNome","Nome non Presente");
+
+            }else if (request.getParameter("Nome").length()>100)
+            {
+                map.put("ErroreNome","Nome troppo lungo");
+            }
+
+            if(request.getParameter("Cognome")==null)
+            {
+                map.put("ErroreCognome","Cognome non Presente");
+            }else if (request.getParameter("Cognome").length()>100)
+            {
+                map.put("ErroreCognome","Cognome troppo lungo");
+            }
+
+            if (map.isEmpty()) {
+                return true;
+            } else {
+
+                refreshPage(request, response, map);
+                return false;
+            }
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+            return false;
+        }
+
+    }
+
+    private void refreshPage(HttpServletRequest request,HttpServletResponse response,Map<String,Object> errori)throws IOException,ServletException
+    {
+        System.out.println("Sto ricaricando l'errore");
+
+        List<String> dati = new ArrayList<>();
+        if(!(errori.containsKey("ErroreEmail")))
+        {  dati.add("Email");}
+
+        if(!(errori.containsKey("ErroreNome")))
+        {
+            dati.add("Nome");
+        }
+        if(!(errori.containsKey("ErroreCognome")))
+        { dati.add("Cognome");}
+
+        if(!(errori.containsKey("ErrorePassword")))
+        {
+            dati.add("Password");
+            dati.add("ConfermaPassword");
+        }
+
+        //ricarico i vecchi dati immessi nella form
+        datamodel.putAll(utility.AddAllData(request,response,dati));
+        //Carico gli avvisi per gli errori trovati
+        datamodel.putAll(errori);
+        System.out.println(datamodel);
+        AdminFillTable page = new AdminFillTable(datamodel,getServletContext(),request,response);
+        page.makeget();
+
+    }
+
+
+
+
+
+
+
+}
