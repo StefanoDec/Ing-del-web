30a02078663fa15e11dc78444c5cfb6aff46a49a
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-02-27 16:31:05 +0100

    Creazione post e modifica get

--- gdellapeProject-master/src/controller/adm/Azienda/GestioneModuliAzienda.java
+++ gdellapeProject-master/src/controller/adm/Azienda/GestioneModuliAzienda.java
@@ -5,15 +5,16 @@ import dao.exception.DaoException;
 import dao.implementation.OffertaTirocinioDaoImp;
 import dao.implementation.TirocinanteDaoImp;
 import dao.implementation.TirocinioDaoImp;
+import dao.implementation.UserDaoImp;
 import model.*;
+import view.TemplateController;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GestioneModuliAzienda {
     protected Map<String, Object> datamodel;
@@ -30,45 +31,124 @@ public class GestioneModuliAzienda {
         this.azienda = null;
     }
 
-    private Azienda ritornaAzienda(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        SingSessionContoller session = SingSessionContoller.getInstance();
-        return session.getAzienda(request,response);
+    private void ritornaAzienda(HttpServletRequest request, HttpServletResponse response){
+        try {
+            SingSessionContoller session = SingSessionContoller.getInstance();
+            this.azienda = session.getAzienda(request,response);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
-    private void fillConvenzione(Azienda azienda){
+    private void fillConvenzione(){
+        //TODO Implementare controllo data di convenzione
         datamodel.put("DataConvenzione", azienda.getDataConvenzione());
         datamodel.put("DataUpdate", azienda.getUpdateDate());
         datamodel.put("IDConvenzione", azienda.getIDAzienda());
     }
 
-    private List<OffertaTirocinio> ritornaOfferte() throws DaoException {
+    private List<OffertaTirocinio> ritornaOfferte(){
         OffertaTirocinioDaoImp daoOfferte = new OffertaTirocinioDaoImp();
-        List<OffertaTirocinio> offertaTirocinios = daoOfferte.getOffertatrBYAzienda(azienda);
-        daoOfferte.destroy();
+        List<OffertaTirocinio> offertaTirocinios = new ArrayList<>();
+        try {
+            offertaTirocinios.addAll(daoOfferte.getOffertatrBYAzienda(azienda));
+            daoOfferte.destroy();
+        }
+        catch (DaoException e ){
+            e.printStackTrace();
+        }
+        //System.out.println(offertaTirocinios.toString());
         return offertaTirocinios;
     }
 
-    private List<Tirocinio> ritornaTirocinii(List<OffertaTirocinio> offertaTirocinios) throws DaoException {
+    private List<Tirocinio> ritornaTirocinii(List<OffertaTirocinio> offertaTirocinios){
         TirocinioDaoImp daoTirocinio = new TirocinioDaoImp();
         List<Tirocinio> listaTirocini = new ArrayList<>();
         for(OffertaTirocinio offertaTirocinio : offertaTirocinios){
-            System.out.println(offertaTirocinio);
-            listaTirocini.addAll(daoTirocinio.getTrByOfferta(offertaTirocinio));
+            try {
+                listaTirocini.addAll(daoTirocinio.getTrByOfferta(offertaTirocinio));
+                daoTirocinio.destroy();
+            } catch (DaoException e) {
+                e.printStackTrace();
+            }
         }
-        daoTirocinio.destroy();
+        //System.out.println(listaTirocini.toString());
         return listaTirocini;
     }
 
-    private List<Tirocinante> ritornaTirocinanti(List<Tirocinio> listaTirocini) throws DaoException {
+    private List<Tirocinante> ritornaTirocinanti(List<Tirocinio> listaTirocini) {
         TirocinanteDaoImp daoTirocinante = new TirocinanteDaoImp();
         List<Tirocinante> listaTirocinanti = new ArrayList<>();
         for (Tirocinio tirocinio: listaTirocini){
-            System.out.println(tirocinio);
-            listaTirocinanti.add(daoTirocinante.getTirocianteByID(tirocinio.getTirocinante()));
+            try {
+                listaTirocinanti.add(daoTirocinante.getTirocianteByID(tirocinio.getTirocinante()));
+                daoTirocinante.destroy();
+            } catch (DaoException e) {
+                e.printStackTrace();
+            }
         }
-        daoTirocinante.destroy();
+        //System.out.println(listaTirocinanti.toString());
         return  listaTirocinanti;
     }
 
+    private void creaDatamodel(List<Tirocinante> tirocinantes, List<OffertaTirocinio> offertaTirocinios, List<Tirocinio> tirocinios){
+        List<Object> lista = new ArrayList<>();
+        int idOfferta = 0;
+        int idTircinante = 0;
+        for (Tirocinio tirocinio : tirocinios){
+            Map<String, Object> mappa = new HashMap<String, Object>();
+            idOfferta = tirocinio.getOffertaTirocinio();
+            idTircinante = tirocinio.getTirocinante();
+
+            mappa.put("tirocinio", tirocinio);
+            for (OffertaTirocinio offertaTirocinio: offertaTirocinios){
+                if (offertaTirocinio.getIDOffertaTirocinio() == idOfferta){
+                    mappa.put("offerta", offertaTirocinio);
+                    break;
+                }
+            }
+            for (Tirocinante tirocinante: tirocinantes){
+                if (tirocinante.getIDTirocinante() == idTircinante){
+                    mappa.put("tirocinante", tirocinante);
+                    UserDaoImp daoUser = new UserDaoImp();
+                    try {
+                        User user = daoUser.getUserByid(tirocinante.getUser());
+                        mappa.put("userMail", user.getEmail());
+                        daoUser.destroy();
+                    } catch (DaoException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+                }
+            }
+            lista.add(mappa);
+        }
+        datamodel.put("Lista", lista);
+
+    }
+    public void get(){
+        ritornaAzienda(request,response);
+        List<OffertaTirocinio> offert = ritornaOfferte();
+        List<Tirocinio> tiro = ritornaTirocinii(offert);
+        List<Tirocinante> tirocinante = ritornaTirocinanti(tiro);
+        creaDatamodel(tirocinante,offert,tiro);
+        fillConvenzione();
+        TemplateController.process("moduli-aziendale.ftl", datamodel, response, context);
+    }
+
+    public void post(){
+        Map params = request.getParameterMap();
+        List<String> parametri = new ArrayList<String>();
+        for (Object o : params.keySet()) {
+            String key = (String) o;
+            String value = ((String[]) params.get(key))[0];
+            if (key.startsWith("fin_") && value.equals("1")){
+                parametri.add(key);
+            }
+        }
+        System.out.println(parametri);
+    }
 
 }
--- gdellapeProject-master/src/controller/adm/BackEndModuliController.java
+++ gdellapeProject-master/src/controller/adm/BackEndModuliController.java
@@ -1,7 +1,7 @@
 package controller.adm;
 
+import controller.adm.Azienda.GestioneModuliAzienda;
 import controller.baseController;
-import view.TemplateController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +14,22 @@ public class BackEndModuliController extends baseController {
         int tipo= (int)request.getAttribute("tipo");
         if (tipo == 3){
             System.out.println("Devo entra dentro la pagina del azienda");
-            TemplateController.process("moduli-aziendale.ftl", datamodel, response, getServletContext());
+            //TemplateController.process("moduli-aziendale.ftl", datamodel, response, getServletContext());
+            GestioneModuliAzienda moduliaz = new GestioneModuliAzienda(datamodel, request, response, getServletContext());
+            System.out.println("creato");
+            moduliaz.get();
+            System.out.println("getttato");
+
         }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)throws  ServletException, IOException{
-
+        super.init(request,response);
+        int tipo= (int)request.getAttribute("tipo");
+        if (tipo == 3){
+            System.out.println("Devo entra dentro la pagina del azienda");
+            GestioneModuliAzienda moduliaz = new GestioneModuliAzienda(datamodel, request, response, getServletContext());
+            moduliaz.post();
+        }
     }
 }
