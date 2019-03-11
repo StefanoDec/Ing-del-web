5feed429d034545703a423e219236e188e0cbf9d
valerio crescia <valerio.crescia@student.univaq.it>
2019-03-11 19:29:25 +0100

    modifiche admin backend

--- gdellapeProject-master/src/controller/adm/Admin/AdminGestioneModuliController.java
+++ gdellapeProject-master/src/controller/adm/Admin/AdminGestioneModuliController.java
@@ -1,6 +1,8 @@
 package controller.adm.Admin;
 
+import controller.adm.Admin.GestioneOfferteTirocinio.FillRichiesteTr;
 import controller.adm.Azienda.BackEndAziendaController;
+import controller.baseController;
 import view.TemplateController;
 
 import javax.servlet.ServletException;
@@ -8,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class AdminGestioneModuliController extends BackEndAziendaController{
+public class AdminGestioneModuliController extends baseController {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
@@ -16,56 +18,11 @@ public class AdminGestioneModuliController extends BackEndAziendaController{
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        super.init(request,response);
+        FillRichiesteTr tr = new FillRichiesteTr();
+        datamodel.putAll(tr.fill(request,response));
         TemplateController.process("BackEndTemplates/gestione-moduli.ftl", datamodel, response, getServletContext());
     }
-    /**protected void fillTablePendenti(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException {
-        try {
-            SingSessionContoller session = SingSessionContoller.getInstance();
-            Azienda azienda = session.getAzienda(request,response);
 
-
-            OffertaTirocinioDaoImp daoof = new OffertaTirocinioDaoImp();
-            List<List<String>> tabella = new ArrayList<>();
-            List<OffertaTirocinio> offertetrAzienda = daoof.getOffertatrBYAzienda(azienda);
-            //Ho tutte le offerte che qualla azienda a fatto
-            daoof.destroy();
-            for (OffertaTirocinio of : offertetrAzienda) {//predo la singola offerta
-
-                TirocinioDaoImp trDao = new TirocinioDaoImp();
-                List<Tirocinio> tr = trDao.getTrByOfferta(of);
-                trDao.destroy();
-
-                for (Tirocinio tr1 : tr) {//prendo il singolo tirocinio
-                    TirocinanteDaoImp daotr = new TirocinanteDaoImp();
-                    Tirocinante tirocinante = daotr.getTirocianteByID(tr1.getTirocinante());
-                    daotr.destroy();
-
-                    UserDaoImp daouser = new UserDaoImp();
-                    User user = daouser.getUserByid(tirocinante.getUser());
-                    daouser.destroy();
-                    List<String> tupla = new ArrayList<>();
-                    tupla.add(tirocinante.getNome());
-                    tupla.add(tirocinante.getCognome());
-                    tupla.add(user.getEmail());
-                    tupla.add(of.getTitolo());
-                    tupla.add(of.getIDOffertaTirocinio().toString());
-                    tupla.add(tr1.getStato().toString());
-                    tupla.add(tr1.getIDTirocinio().toString());
-                    tabella.add(tupla);
-
-                }
-            }
-            for (List<String> lista1: tabella){
-                for(String lista2 : lista1){
-                    System.out.println(lista2);
-                }
-            }
-
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }**/
 
 }
--- gdellapeProject-master/src/controller/adm/Admin/BackEndAdminController.java
+++ gdellapeProject-master/src/controller/adm/Admin/BackEndAdminController.java
@@ -2,40 +2,139 @@ package controller.adm.Admin;
 
 import controller.baseController;
 import controller.sessionController.SingSessionContoller;
+import dao.exception.DaoException;
+import dao.implementation.*;
+import model.*;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.rmi.ServerException;
 
-public class BackEndAdminController extends baseController{
-    protected Boolean accessLogin(HttpServletRequest request, HttpServletResponse response)throws IOException,ServerException{
-        SingSessionContoller session = SingSessionContoller.getInstance();
-        if(!(session.isValidSession(request))){
+public class BackEndAdminController extends HttpServlet {
 
-                response.sendRedirect("/login");
-                return false;
+    public static Tirocinante getTirocinante(HttpServletRequest request,HttpServletResponse response,int idtr) throws IOException, ServletException
+    {
+        Tirocinante tirocinante=  new Tirocinante();
 
-        }else{return true;}
-    }
-    protected Boolean accessAzienda(HttpServletRequest request, HttpServletResponse response)throws IOException,ServerException{
-        SingSessionContoller session = SingSessionContoller.getInstance();
-        if( !(session.isValidSession(request))){
-
-            response.sendRedirect("/401");
-            return false;
-
-        }else{return true;}
-    }
-
-    protected void init(HttpServletRequest request,HttpServletResponse response)throws IOException,ServerException{
-      try {
-          super.init(request,response);
-
-      }catch (Exception e){
-          e.printStackTrace();
-      }
-
+        try {
+            TirocinanteDaoImp dao = new TirocinanteDaoImp();
+            Tirocinante tr = dao.getTirocianteByID(idtr);
+            dao.destroy();
+            tirocinante=tr;
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return tirocinante;
 
     }
+
+    public static User getUser(HttpServletRequest request, HttpServletResponse response,int iduser) throws IOException, ServletException
+    {
+        User user= new User();
+
+        try {
+            UserDaoImp dao = new UserDaoImp();
+            user=dao.getUserByid(iduser);
+            dao.destroy();
+
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return user;
+
+    }
+    public static Admin getAdmin(HttpServletRequest request, HttpServletResponse response,int idAdmin) throws IOException, ServletException
+    {
+       Admin admin= new Admin();
+
+        try {
+            AdminDaoImp dao = new AdminDaoImp();
+            admin = dao.getAdminByID(idAdmin);
+            dao.destroy();
+
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return admin;
+
+
+    }
+    public static Azienda getAzienda(HttpServletRequest request, HttpServletResponse response,int idAzienda)throws IOException,ServletException
+    {
+        Azienda azienda= new Azienda();
+
+        try {
+          AziendaDaoImp dao = new AziendaDaoImp();
+           azienda=dao.getAziendaByID(idAzienda);
+           dao.destroy();
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return azienda;
+
+    }
+    public static Tirocinio getTirocinio(HttpServletRequest request, HttpServletResponse response,int idTirocinio)throws IOException,ServletException
+    {
+       Tirocinio tirocinio=new Tirocinio();
+
+        try {
+           TirocinioDaoImp dao = new TirocinioDaoImp();
+           tirocinio=dao.getRichiestatrByID(idTirocinio);
+           dao.destroy();
+
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return tirocinio;
+
+    }
+    public static TutoreUniversitario getTutoreUni(HttpServletRequest request, HttpServletResponse response,int idtut)throws IOException,ServletException
+    {
+        TutoreUniversitario tutuni=new TutoreUniversitario();
+
+        try {
+            TutoreUniversitarioDaoImp dao= new TutoreUniversitarioDaoImp();
+            tutuni=dao.getTutoreUniByID(idtut);
+            dao.destroy();
+
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return tutuni;
+
+    }
+    public static OffertaTirocinio getOffertaTr(HttpServletRequest request, HttpServletResponse response,int idOf)throws IOException,ServletException
+    {
+       OffertaTirocinio Offtirocinio=new OffertaTirocinio();
+
+        try {
+           OffertaTirocinioDaoImp dao = new OffertaTirocinioDaoImp();
+           Offtirocinio = dao.getOffertatrByID(idOf);
+           dao.destroy();
+
+        }catch (DaoException e)
+        {
+            e.printStackTrace();
+            response.sendRedirect("/404");
+        }
+        return Offtirocinio;
+
+    }
+
+
 }
