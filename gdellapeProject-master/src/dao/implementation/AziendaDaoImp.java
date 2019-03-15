35b3cf7dab09e0647d5fb5536d32b7941117f6d3
valerio crescia <valerio.crescia@student.univaq.it>
2019-03-15 11:25:32 +0100

    Add function "getAllAziendaAttiva" and property "selectAllAziendaAttiva"

--- gdellapeProject-master/src/dao/implementation/AziendaDaoImp.java
+++ gdellapeProject-master/src/dao/implementation/AziendaDaoImp.java
@@ -19,7 +19,7 @@ public class AziendaDaoImp extends DaoDataMySQLImpl {
     private PreparedStatement insertAzienda;
     private PreparedStatement selectAllAzienda;
     private PreparedStatement selectAziendaByID;
-    private PreparedStatement selectAllConvenzione;
+    private PreparedStatement selectAllAziendaAttiva;
     private PreparedStatement selectLastFiveConvenzioni;
     private PreparedStatement selectAziendaByIDuser;
     private PreparedStatement regAzienda;
@@ -48,8 +48,8 @@ public class AziendaDaoImp extends DaoDataMySQLImpl {
             this.selectAllPendentAzieda = connection.prepareStatement
                     ("SELECT * FROM azienda WHERE Attivo=0 ORDER BY UpdateDate ASC");
 
-            this.selectAllConvenzione = connection.prepareStatement
-                    ("SELECT * FROM azienda ORDER BY UpdateDate ASC");
+            this.selectAllAziendaAttiva = connection.prepareStatement
+                    ("SELECT * FROM azienda WHERE Attivo=1 ORDER BY UpdateDate ASC");
 
 
             this.selectLastFiveConvenzioni = connection.prepareStatement
@@ -243,11 +243,11 @@ public class AziendaDaoImp extends DaoDataMySQLImpl {
         return aziende;
     }
 
-    public List<Azienda> getAllConvenzione() throws DaoException {
+    public List<Azienda> getAllAziendeAttive() throws DaoException {
         List<Azienda> convenzioni = new ArrayList<>();
         try {
             this.init();
-            ResultSet resultSet = this.selectAllConvenzione.executeQuery();
+            ResultSet resultSet = this.selectAllAziendaAttiva.executeQuery();
             setListAzienda(convenzioni, resultSet);
         } catch (SQLException e) {
             throw new DaoException("Errore query", e);
@@ -341,7 +341,7 @@ public class AziendaDaoImp extends DaoDataMySQLImpl {
             this.insertAzienda.close();
             this.selectAllAzienda.close();
             this.selectAziendaByID.close();
-            this.selectAllConvenzione.close();
+            this.selectAllAziendaAttiva.close();
             this.selectLastFiveConvenzioni.close();
             this.selectAziendaByIDuser.close();
             this.regAzienda.close();
