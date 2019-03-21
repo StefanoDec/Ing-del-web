5a3334ad1aa09f0c6ef255134549e332b4925e2a
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-03-21 17:39:07 +0100

    Cambio nome cartella FineTirocinio --> FineTirocinioAzienda

--- gdellapeProject-master/src/controller/utility/UploadFilePDF.java
+++ gdellapeProject-master/src/controller/utility/UploadFilePDF.java
@@ -19,7 +19,7 @@ public class UploadFilePDF extends baseController {
      * @param request  richiesta
      * @param savePath albero cartelle Convenzione ./PDF/Covenzione/IDAzienda/NOMEPDF.pdf
      *                 albero cartelle Richiesta di tirocinio ./PDF/RichestaTirocinio/IDTirocinio/IDTirocininate/NOMEPDF.pdf
-     *                 albero cartella Fine Tirocinio Azienda ./PDF/FineTirocinio/IDTirocinio/IDTirocininante/NOMEPDF.pdf
+     *                 albero cartella Fine Tirocinio Azienda ./PDF/FineTirocinioAzienda/IDTirocinio/IDTirocininante/NOMEPDF.pdf
      *                 albero cartella Segreteria ./PDF/Segreteria/IDTirocinio/IDTirocininante/NOMEPDF.pdf
      * @param part     variabile di tipo Part del file che si ricava tramite:
      *                 Part parto = request.getPart("NOME FORM");
