cbc4f842dee004a9676959ca79b5aed6743574c4
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-03-28 14:39:48 +0100

    Debug

--- gdellapeProject-master/src/controller/errori/Errore500Controller.java
+++ gdellapeProject-master/src/controller/errori/Errore500Controller.java
@@ -1,4 +1,5 @@
 package controller.errori;
+
 import controller.baseController;
 import view.TemplateController;
 
@@ -19,11 +20,21 @@ public class Errore500Controller extends baseController {
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         String servletName = (String) request.getAttribute("javax.servlet.error.servlet_name");
         Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-        datamodel.put("StatusCode", statusCode);
-        datamodel.put("ServeletName", servletName);
-        datamodel.put("Throwable", throwable.getMessage());
-        datamodel.put("ThrowableStackTrace",  throwable.getStackTrace());
-        datamodel.put("ThrowableCause", throwable.getCause());
+        if (statusCode != null){
+            datamodel.put("StatusCode", statusCode);
+        }
+        if (servletName != null){
+            datamodel.put("ServeletName", servletName);
+        }
+        if (throwable.getMessage() != null) {
+            datamodel.put("Throwable", throwable.getMessage());
+        }
+        if (throwable.getStackTrace() != null){
+            datamodel.put("ThrowableStackTrace", throwable.getStackTrace());
+        }
+        if (throwable.getCause() != null){
+            datamodel.put("ThrowableCause", throwable.getCause());
+        }
         TemplateController.process("500.ftl", datamodel, response, getServletContext());
 
     }
