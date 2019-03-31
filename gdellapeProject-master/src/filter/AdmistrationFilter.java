3f2339f526bef68fc7d131538c6d198b7e7ed5f6
valerio crescia <valerio.crescia@student.univaq.it>
2019-03-31 15:51:21 +0200

    litraggio solo admin

--- gdellapeProject-master/src/filter/AdmistrationFilter.java
+++ gdellapeProject-master/src/filter/AdmistrationFilter.java
@@ -4,23 +4,27 @@ import javax.servlet.*;
 import java.io.IOException;
 
 public class AdmistrationFilter implements Filter {
+    private ServletContext context;
+
+    public void init(FilterConfig config) throws ServletException {
+     this.context=config.getServletContext();
+
+    }
+
     public void destroy() {
     }
-//Todo prova se funziona
+    //Todo prova se funziona l'errore
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        if(Integer.parseInt(req.getParameter("tipo"))==1)
+        if(req.getAttribute("tipo").equals(1))
         {
             chain.doFilter(req, resp);
         }else{
-            RequestDispatcher requestDispatcher= req.getRequestDispatcher("/403");
-            requestDispatcher.forward(req,resp);
+           RequestDispatcher requestDispatcher= context.getRequestDispatcher("/500");
+           requestDispatcher.forward(req,resp);
         }
 
-
     }
 
-    public void init(FilterConfig config) throws ServletException {
 
-    }
 
 }
