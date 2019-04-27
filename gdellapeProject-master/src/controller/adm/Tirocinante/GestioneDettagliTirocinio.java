f48c5e6fde53c2551c4147e2cfad8e142c8a1914
Stefano Decina <stefano.decina@student.univaq.it>
2019-04-27 13:32:58 +0200

    caricaVotazioneSuTirocinio caricaVotazioneSuAzienda eccezioni debaug

--- gdellapeProject-master/src/controller/adm/Tirocinante/GestioneDettagliTirocinio.java
+++ gdellapeProject-master/src/controller/adm/Tirocinante/GestioneDettagliTirocinio.java
@@ -10,6 +10,7 @@ import model.OffertaTirocinio;
 import model.Tirocinio;
 import view.TemplateController;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -42,67 +43,77 @@ public class GestioneDettagliTirocinio extends baseController {
         return azienda;
     }
 
-    private void fillDatamodel(HttpServletRequest request) throws DaoException {
-        Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
-        datamodel.put("offertaTirocinio", ritornaOffertaTirocinio(idTirocinio));
-        datamodel.put("azienda", ritornaAzienda(ritornaOffertaTirocinio(idTirocinio)));
-        datamodel.put("tirocinio", ritornaTirocinio(idTirocinio));
+    private void fillDatamodel(HttpServletRequest request) {
+        try {
+            Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
+            datamodel.put("offertaTirocinio", ritornaOffertaTirocinio(idTirocinio));
+            datamodel.put("azienda", ritornaAzienda(ritornaOffertaTirocinio(idTirocinio)));
+            datamodel.put("tirocinio", ritornaTirocinio(idTirocinio));
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
     }
 
 
     private float votazione(HttpServletRequest request) throws DaoException {
-        int valutazioneTutoraggio = Integer.parseInt(request.getParameter("valutazioneTutoraggio"));
-        Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
-        Azienda azienda = ritornaAzienda(ritornaOffertaTirocinio(idTirocinio));
-        float totaleVoti = azienda.getValutazione() * azienda.getVoti();
-        totaleVoti = totaleVoti + valutazioneTutoraggio;
-        totaleVoti = totaleVoti / ((azienda.getVoti() + 1));
-        return (float) (Math.round( totaleVoti * Math.pow( 10, 2 ) )/Math.pow( 10, 2 ));
+
+            int valutazioneTutoraggio = Integer.parseInt(request.getParameter("valutazioneTutoraggio"));
+            Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
+            Azienda azienda = ritornaAzienda(ritornaOffertaTirocinio(idTirocinio));
+            float totaleVoti = azienda.getValutazione() * azienda.getVoti();
+            totaleVoti = totaleVoti + valutazioneTutoraggio;
+            totaleVoti = totaleVoti / ((azienda.getVoti() + 1));
+            return (float) (Math.round(totaleVoti * Math.pow(10, 2)) / Math.pow(10, 2));
     }
 
-    private void caricaVotazioneSuAzienda(HttpServletRequest request) throws DaoException {
-        Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
-        Azienda azienda = ritornaAzienda(ritornaOffertaTirocinio(idTirocinio));
-        azienda.setValutazione(votazione(request));
-        int voti = azienda.getVoti();
-        azienda.setVoti(voti + 1);
-        AziendaDaoImp daoAzienda = new AziendaDaoImp();
-        daoAzienda.updateAzienda(azienda);
-        daoAzienda.destroy();
+    private void caricaVotazioneSuAzienda(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
+            Azienda azienda = ritornaAzienda(ritornaOffertaTirocinio(idTirocinio));
+            azienda.setValutazione(votazione(request));
+            int voti = azienda.getVoti();
+            azienda.setVoti(voti + 1);
+            AziendaDaoImp daoAzienda = new AziendaDaoImp();
+            daoAzienda.updateAzienda(azienda);
+            daoAzienda.destroy();
+        } catch (DaoException e) {
+            e.printStackTrace();
+            RequestDispatcher page = request.getRequestDispatcher("/500");
+            page.forward(request, response);
+        }
     }
 
-    private void caricaVotazioneSuTirocinio(HttpServletRequest request) throws DaoException {
-        Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
-        Tirocinio tirocinio = ritornaTirocinio(idTirocinio);
-        tirocinio.setStato(4);
-        TirocinioDaoImp daoTirocinio = new TirocinioDaoImp();
-        System.out.println("Tirocinio votato");
-        System.out.println(tirocinio);
-        daoTirocinio.updateTirocinio(tirocinio);
-        daoTirocinio.destroy();
+    private void caricaVotazioneSuTirocinio(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Integer idTirocinio = Integer.parseInt(request.getParameter("idTirocinio"));
+            Tirocinio tirocinio;
+            tirocinio = ritornaTirocinio(idTirocinio);
+            tirocinio.setStato(4);
+            TirocinioDaoImp daoTirocinio = new TirocinioDaoImp();
+            System.out.println("Tirocinio votato");
+            System.out.println(tirocinio);
+            daoTirocinio.updateTirocinio(tirocinio);
+            daoTirocinio.destroy();
+
+        } catch (DaoException e) {
+            e.printStackTrace();
+            RequestDispatcher page = request.getRequestDispatcher("/500");
+            page.forward(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.init(request, response);
-        try {
-            fillDatamodel(request);
-            TemplateController.process("tirocinio-tirocinante.ftl", datamodel, response, getServletContext());
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
+        fillDatamodel(request);
+        TemplateController.process("tirocinio-tirocinante.ftl", datamodel, response, getServletContext());
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.init(request, response);
-        try {
-            fillDatamodel(request);
-            caricaVotazioneSuTirocinio(request);
-            caricaVotazioneSuAzienda(request);
-            response.sendRedirect("/account/moduli");
-//            TemplateController.process("tirocinio-tirocinante.ftl", datamodel, response, getServletContext());
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
+        fillDatamodel(request);
+        caricaVotazioneSuTirocinio(request, response);
+        caricaVotazioneSuAzienda(request, response);
+        response.sendRedirect("/account/moduli");
     }
 
 }
