820391ef3326bcaf2d8d6f21200a93ce46292c73
Stefano Decina <stefano.decina@student.univaq.it>
2019-04-27 13:31:13 +0200

    debug

--- gdellapeProject-master/src/controller/adm/Tirocinante/BackEndTrController.java
+++ gdellapeProject-master/src/controller/adm/Tirocinante/BackEndTrController.java
@@ -2,37 +2,37 @@ package controller.adm.Tirocinante;
 import controller.baseController;
 import controller.sessionController.SingSessionContoller;
 
-import javax.naming.Context;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.rmi.ServerException;
 
 public class BackEndTrController extends baseController {
-    protected Boolean loginTirocinante(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException,ServletException {
+     protected Boolean loginTirocinante(HttpServletRequest request, HttpServletResponse response) throws IOException {
         SingSessionContoller session = SingSessionContoller.getInstance();
         if(!(session.isValidSession(request))) {
           response.sendRedirect("/login");
           return false;
         }else{return true;}
     }
-    protected Boolean accessTirocinante(HttpServletRequest request,HttpServletResponse response) throws ServerException,IOException,ServletException{
+   protected Boolean accessTirocinante(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         SingSessionContoller session = SingSessionContoller.getInstance();
         if(!(session.isTirocinante(request))){
-            response.sendRedirect("/401");
+            RequestDispatcher page = request.getRequestDispatcher("/401");
+            page.forward(request,response);
             return false;
 
         }else{return true;}
     }
-    protected void init(HttpServletRequest request,HttpServletResponse response)throws ServerException,IOException{
+    protected void init(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
         try{
             super.init(request,response);
 
         }catch (Exception e){
             e.printStackTrace();
-            //devi mettere errore 500
+            RequestDispatcher page = request.getRequestDispatcher("/500");
+            page.forward(request,response);
         }
     }
 }
